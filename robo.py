03a2fdbb9678141c339bc4162af5b446198b8757
joao marcos lacerda dos santos <104016774+joaomarcosls@users.noreply.github.com>
2022-05-19 23:44:05 -0300

    Initial commit

---
+++ robo.py
@@ -0,0 +1,51 @@
+from chatterbot import ChatBot
+from difflib import SequenceMetcher 
+
+CONFIANCA_MINIMA = 0.50
+
+
+def  comparar_mensagens(mensagen_digitada, mensagen_candidata):
+    confianca = 0.0
+    
+    digitada = mensagen_digitada.text
+    candidata = mensagen_candidata.text
+    if digitada and candidata:
+        SequenceMetcher(None,
+                        digitada,
+                        candidata)
+    confianca = round(confianca.ratio(), 2)
+    
+    
+    return confianca
+
+
+
+def iniciar():
+    robo = ChatBot("Robô de Atendimento do IFBA",
+                   read_only=True,
+                   statement_comparison_function=comparar_mensagens
+                   logic_adapters=[
+                       {
+                           "import_path": "chatterbot.logic.BestMatch",
+                       }
+                   ])
+
+    return robo
+
+
+def executar_robo(robo):
+    while True:
+        mensagem = input("Digite alguma coisa... \n")
+        resposta = robo.get_response(mensagem.lower())
+        print(f"o valor da confiança é: {resposta.confidence}")
+        if resposta.confidence >= CONFIANCA_MINIMA:
+            print(">>", resposta.text)
+        else:
+            print("Infelizmente, ainda não sei responder isso")
+            print("Pergunte outra coisa")
+
+
+if __name__ == "__main__":
+    robo = iniciar()
+
+    executar_robo(robo)
