d0963fbc65f35a766e9b98a32c4a0ed26942cb57
João Marcos Lacerda <marcoslacerda99.jmls@gmail.com>
2022-09-05 21:39:19 -0300

    aula segunda parte

--- processar_texto.py
+++ processar_texto.py
@@ -1,7 +1,22 @@
 from lib2to3.pgen2 import token
-from nltk import word_tokenize
+from nltk import word_tokenize, corpus
 
 LINGUAGEM = "portuguese"
+
+def get_palavras_de_parada():
+    palavras_de_parada = set(corpus.stopwords.words(LINGUAGEM))
+
+    return palavras_de_parada
+
+def remover_palaras_de_parada(tokens):
+    tokens_filtrados= []
+    palavras_de_parada = get_palavras_de_parada:
+
+    for token in tokens:
+        if token not in palavras_de_parada:
+            tokens_filtrados.append(token)
+
+    return tokens_filtrados
 
 
 def obter_tokens(texto):
@@ -19,3 +34,6 @@ if __name__ == "__main__":
 
 tokens = obter_tokens(texto)
 imprimir_tokens(tokens)
+
+
+remover_palaras_de_parada()
