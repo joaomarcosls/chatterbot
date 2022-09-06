3e5cf130b386ebceac775b4e0b1c8c1e38bf0d97
João Marcos Lacerda <marcoslacerda99.jmls@gmail.com>
2022-09-05 21:25:13 -0300

    aula 1

---
+++ processar_texto.py
@@ -0,0 +1,21 @@
+from lib2to3.pgen2 import token
+from nltk import word_tokenize
+
+LINGUAGEM = "portuguese"
+
+
+def obter_tokens(texto):
+    tokens = word_tokenize(texto, LINGUAGEM)
+
+    return tokens
+
+def imprimir_tokens(tokens):
+    for token in tokens:
+        print(f"token: {token} ")
+
+
+if __name__ == "__main__":
+    texto = "a verdadeira generosidade para com o futuro consiste em dar tudo ao presente"
+
+tokens = obter_tokens(texto)
+imprimir_tokens(tokens)
