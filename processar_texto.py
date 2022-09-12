5f010cae7dd6efc34038fe00544276fe81c02761
João Marcos Lacerda dos Santos <104016774+joaomarcosls@users.noreply.github.com>
2022-09-12 19:29:34 -0300

    anna

--- processar_texto.py
+++ processar_texto.py
@@ -1,5 +1,6 @@
 from lib2to3.pgen2 import token
 from nltk import word_tokenize, corpus
+from nltk.corpus import floresta 
 
 LINGUAGEM = "portuguese"
 
@@ -11,6 +12,7 @@ def get_palavras_de_parada():
 def remover_palaras_de_parada(tokens):
     tokens_filtrados= []
     palavras_de_parada = get_palavras_de_parada:
+    tokens_filtrados.
 
     for token in tokens:
         if token not in palavras_de_parada:
@@ -23,6 +25,19 @@ def obter_tokens(texto):
     tokens = word_tokenize(texto, LINGUAGEM)
 
     return tokens
+
+def get_classificar_gramaticalmente():
+      classificacoes = []
+
+for (palavras, classificacao) in floresta.tagged_words():
+ if "+" in classificacao:
+      classificacao = classificacao[classificacao.index("+")+1:]
+      
+    return classificacoes
+
+def classificar_gramaticalmente(tokens):
+    pass 
+
 
 def imprimir_tokens(tokens):
     for token in tokens:
